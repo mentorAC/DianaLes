78de419b1dd7b9cb596d56919f75e49022e115d0
maxpe3447 <m.pech.pr@gmail.com>
2022-12-30 10:00:37 +0200

    les 30/12

--- HappyNewYear/Source.cpp
+++ HappyNewYear/Source.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
-
+#include <ctime>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	tm tim = tm();
+	tim.tm_mday = 1;
+	tim.tm_mon = 1-1;
+	tim.tm_year = 2023 - 1900;
+	time_t new_year = mktime(&tim);
 
+	while (true) {
+		time_t carlday = time(0);
+		double sec = difftime(new_year, carlday);
+
+		if (sec == 0) {
+			break;
+		}
+
+		int dni = sec / 60 / 60 / 24;
+		int god = sec / 60 / 60 - 24 * dni;
+		int hwyl = sec / 60 - 60 * god - dni * 24 * 60;
+		int sec1 = sec - 60 * god * 60 - hwyl * 60 - dni * 24 * 60 * 60;
+
+		cout << dni << " Days " << god << ":" << hwyl << ":" << sec1 << "\r\t";
+		//Sleep(500);
+		//system("cls");
+	}
+	cout << "Happy new year!";
 	return 0;
 }
